5cdc23d8372468cb26d7bd29032325145b4279ef
qiliang.sun <qiliang.sun@innoic.com>
2019-08-30 17:44:23 +0800

    11

--- onnx_example/onnx_run2.py
+++ onnx_example/onnx_run2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import onnx
-# import onnxruntime.backend as backend
-import caffe2.python.onnx.backend as backend
+import onnxruntime.backend as backend
+# import caffe2.python.onnx.backend as backend
 import os,sys
 
 if len(sys.argv) != 2:
@@ -12,7 +12,7 @@ model = onnx.load(sys.argv[1])
 session = backend.prepare(model)
 
 # get input data
-x = np.random.randn(1, 3, 224, 224).astype(np.float32) * 100
+x = np.random.randn(1, 3, 227, 227).astype(np.float32) * 100
 print(x)
 
 # Run the model on the backend
